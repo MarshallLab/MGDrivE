569e57074972bb1001c1d337424dc3460b35fdc6
Jared Bennett <jared_bennett@berkeley.edu>
2019-06-28 12:08:09 -0700

    Update MGDrivE-Kernels.cpp

--- MGDrivE/src/MGDrivE-Kernels.cpp
+++ MGDrivE/src/MGDrivE-Kernels.cpp
@@ -55,6 +55,10 @@ inline double dtruncExp(double x, double r, double a, double b){
 //' @param sdlog log standard deviation of \code{\link[stats]{Lognormal}} distribution
 //'
 //' @examples
+//' \dontshow{
+//' Examples for users.
+//' Executable in < 5 seconds.
+//' }
 //' # setup distance matrix
 //' # two-column matrix with latitude/longitude, in degrees
 //' latLong = cbind(runif(n = 5, min = 0, max = 90),
@@ -99,6 +103,10 @@ Rcpp::NumericMatrix calcLognormalKernel(const Rcpp::NumericMatrix& distMat,
 //' @param rate rate parameter of \code{\link[stats]{GammaDist}} distribution
 //'
 //' @examples
+//' \dontshow{
+//' Examples for users.
+//' Executable in < 5 seconds.
+//' }
 //' # setup distance matrix
 //' # two-column matrix with latitude/longitude, in degrees
 //' latLong = cbind(runif(n = 5, min = 0, max = 90),
@@ -141,6 +149,10 @@ Rcpp::NumericMatrix calcGammaKernel(const Rcpp::NumericMatrix& distMat, const do
 //' @param rate rate parameter of \code{\link[stats]{Exponential}} distribution
 //'
 //' @examples
+//' \dontshow{
+//' Examples for users.
+//' Executable in < 5 seconds.
+//' }
 //' # setup distance matrix
 //' # two-column matrix with latitude/longitude, in degrees
 //' latLong = cbind(runif(n = 5, min = 0, max = 90),
@@ -186,6 +198,10 @@ Rcpp::NumericMatrix calcExpKernel(const Rcpp::NumericMatrix& distMat, const doub
 //' @param pi point mass at zero
 //'
 //' @examples
+//' \dontshow{
+//' Examples for users.
+//' Executable in < 5 seconds.
+//' }
 //' # setup distance matrix
 //' # two-column matrix with latitude/longitude, in degrees
 //' latLong = cbind(runif(n = 5, min = 0, max = 90),
